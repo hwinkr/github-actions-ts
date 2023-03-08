17b564d4aa0a664c9cd89f76aeee1bf08a588aba
hwinkr <dnddl8280@naver.com>
2023-03-08 21:32:29 +0900

    break the test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 // TODO : factorial function test && use github actions
 export const factorial = (n:number) : number => {
-    if (n < 0){
-        return -1
-    }
-
     if (n == 0) return 1
 
     return n * factorial(n - 1)
 }
-
-// hello
