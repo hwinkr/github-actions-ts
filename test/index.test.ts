e6d56955470ced2c48c8513cfbf8321794d48df8
hwinkr <dnddl8280@naver.com>
2023-03-08 19:44:28 +0900

    test

---
+++ test/index.test.ts
@@ -0,0 +1,13 @@
+import {factorial} from "../src/index"
+import {describe, expect, test} from '@jest/globals';
+
+describe("test add function", () => {
+  test("should return 6 for factorial(3)", () => {
+    expect(factorial(3)).toBe(6);
+  });
+  test("should return error", () => {
+    expect(factorial(-1)).toBe(-1)
+  });
+});
+
+// hello
